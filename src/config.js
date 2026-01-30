2dbdda490ae0c4f9abf7eeb30b59d5c55f41fd82
lineryyds <lineryyds@foxmail.com>
2026-01-31 02:44:16 +0800

    移除无用字体文件

--- src/config.js
+++ src/config.js
@@ -2,10 +2,7 @@ const fonts = [
     [import.meta.env.VITE_APP_FONT_FAMILY, '默认全局'],
     ['Microsoft YaHei', '微软雅黑'],
     // [字体名称, 显示名称, CSS文件地址]
-    ['HarmonyOS_Regular', '鸿蒙字体', 'ttf/hm/result.css'],
-    ['yozai', '悠哉字体', 'ttf/yz/result.css'],
     ['寒蝉全圆体', '寒蝉全圆体', 'ttf/hcqy/result.css'],
-    ['Douyin Sans', '抖音美好体', 'ttf/dymh/result.css'],
     ['MaokenZhuyuanTi', '猫啃珠圆体', 'ttf/mkzy/result.css']
 ];
 
